026e4fcebc94a12c418f8685f969965266ea0e60
hachoj <hjchojnowski@gmail.com>
2024-10-29 23:16:12 -0400

    deleted some files

--- ViT/plt.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-from collections import OrderedDict
-
-test = OrderedDict({"w": [0, 1, 2, 3], "b": [0, 0.5, 1, 1.5]})
-plt.plot(test["w"])
-plt.plot(test["b"])
-plt.savefig("ViT/plots/test.png")
